a800903d732b0dda75ec2cd3aaf31739ffdd097c
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2018-08-05 20:33:39 +0300

    Addition of Toolbar to QuestionsList screen
    Signed-off-by: Alvin Dizon <alvin.dizon@ghl.com>

--- app/src/main/java/com/techyourchance/mvc/screens/common/ViewMvcFactory.java
+++ app/src/main/java/com/techyourchance/mvc/screens/common/ViewMvcFactory.java
@@ -4,12 +4,13 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import com.techyourchance.mvc.screens.common.toolbar.ToolbarViewMvc;
 import com.techyourchance.mvc.screens.questiondetails.QuestionDetailsViewMvc;
 import com.techyourchance.mvc.screens.questiondetails.QuestionDetailsViewMvcImpl;
-import com.techyourchance.mvc.screens.questionslist.questionslistitem.QuestionsListItemViewMvc;
-import com.techyourchance.mvc.screens.questionslist.questionslistitem.QuestionsListItemViewMvcImpl;
 import com.techyourchance.mvc.screens.questionslist.QuestionsListViewMvc;
 import com.techyourchance.mvc.screens.questionslist.QuestionsListViewMvcImpl;
+import com.techyourchance.mvc.screens.questionslist.questionslistitem.QuestionsListItemViewMvc;
+import com.techyourchance.mvc.screens.questionslist.questionslistitem.QuestionsListItemViewMvcImpl;
 
 /**
  * This class will have the Activity's LayoutInflater passed to it (see BaseActivity)
@@ -35,5 +36,9 @@ public class ViewMvcFactory {
 
     public QuestionDetailsViewMvc getQuestionDetailsViewMvc(@Nullable ViewGroup parent) {
         return new QuestionDetailsViewMvcImpl(layoutInflater, parent);
+    }
+
+    public ToolbarViewMvc getToolbarViewMvc(@Nullable ViewGroup parent) {
+        return new ToolbarViewMvc(layoutInflater, parent);
     }
 }
--- app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListViewMvcImpl.java
+++ app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListViewMvcImpl.java
@@ -2,6 +2,7 @@ package com.techyourchance.mvc.screens.questionslist;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +10,9 @@ import android.widget.ProgressBar;
 
 import com.techyourchance.mvc.R;
 import com.techyourchance.mvc.questions.Question;
-import com.techyourchance.mvc.screens.common.views.BaseObservableViewMvc;
 import com.techyourchance.mvc.screens.common.ViewMvcFactory;
+import com.techyourchance.mvc.screens.common.toolbar.ToolbarViewMvc;
+import com.techyourchance.mvc.screens.common.views.BaseObservableViewMvc;
 
 import java.util.List;
 
@@ -19,6 +21,9 @@ public class QuestionsListViewMvcImpl extends BaseObservableViewMvc<QuestionsLis
     private RecyclerView recyclerQuestions;
     private QuestionsRecylerAdapter adapter;
     private ProgressBar progressBar;
+    // nested MVC views
+    private Toolbar toolbar;
+    private ToolbarViewMvc toolbarViewMvc;
 
     public QuestionsListViewMvcImpl(LayoutInflater inflater, ViewGroup viewGroup, ViewMvcFactory viewMvcFactory) {
         setRootView(inflater.inflate(R.layout.layout_questions_list, viewGroup, false));
@@ -28,6 +33,11 @@ public class QuestionsListViewMvcImpl extends BaseObservableViewMvc<QuestionsLis
         recyclerQuestions.setLayoutManager(new LinearLayoutManager(getContext()));
         adapter = new QuestionsRecylerAdapter(this, viewMvcFactory);
         recyclerQuestions.setAdapter(adapter);
+
+        toolbar = findViewById(R.id.include);
+        toolbarViewMvc = viewMvcFactory.getToolbarViewMvc(toolbar);
+        toolbarViewMvc.setTitle(getString(R.string.questions_list_screen_title));
+        toolbar.addView(toolbarViewMvc.getRootView());
     }
 
 
