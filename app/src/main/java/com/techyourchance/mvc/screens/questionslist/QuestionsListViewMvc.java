78e940de5feea060e17de0408ceec9bd1bc5cd00
Alvin Dizon <alvin.dizon91@gmail.com>
2019-05-16 17:37:47 +0800

    Extract root view logic to common ViewMvc interface
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListViewMvc.java
+++ app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListViewMvc.java
@@ -1,18 +1,15 @@
 package com.techyourchance.mvc.screens.questionslist;
 
-import android.view.View;
-
 import com.techyourchance.mvc.questions.Question;
+import com.techyourchance.mvc.screens.common.ViewMvc;
 
 import java.util.List;
 
-interface QuestionsListViewMvc {
+interface QuestionsListViewMvc extends ViewMvc {
     // mechanism for sending feedback to this view's controller (i.e., the activity)
     void registerListener(Listener listener);
 
     void unregisterListener(Listener listener);
-
-    View getRootView();
 
     void bindQuestions(List<Question> questions);
 
