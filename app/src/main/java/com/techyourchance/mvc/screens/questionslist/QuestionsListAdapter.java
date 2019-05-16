4627e9e0135151d3de9ac0b5b9d69ab45c9dbde4
Alvin Dizon <alvin.dizon91@gmail.com>
2019-05-16 15:20:36 +0800

    Use ViewHolder class to hold reference to TextView in ListView adapter
    - this minimizes findViewById lookups when getView is called, making UI
    more responsive and fluid
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListAdapter.java
+++ app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListAdapter.java
@@ -26,19 +26,27 @@ public class QuestionsListAdapter extends ArrayAdapter<Question> {
         mOnQuestionClickListener = onQuestionClickListener;
     }
 
+    // create inner class so that findViewById lookups are minimized whenever getView() is called
+    private static class ViewHolder {
+        private TextView textTitle;
+    }
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         if (convertView == null) {
             convertView = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.layout_question_list_item, parent, false);
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.textTitle = convertView.findViewById(R.id.txt_title);
+            convertView.setTag(viewHolder);
         }
 
         final Question question = getItem(position);
 
         // bind the data to views
-        TextView txtTitle = convertView.findViewById(R.id.txt_title);
-        txtTitle.setText(question.getTitle());
+        ViewHolder viewHolder = (ViewHolder) convertView.getTag();
+        viewHolder.textTitle.setText(question.getTitle());
 
         // set listener
         convertView.setOnClickListener(new View.OnClickListener() {
