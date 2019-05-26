75df38750b161019017ec7a13cfb2abe70302768
Alvin Dizon <alvin.dizon91@gmail.com>
2019-05-26 17:36:30 +0800

    Add code for composition root that is tied to Activity lifecycle
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

---
+++ app/src/main/java/com/techyourchance/mvc/common/di/ControllerCompositionRoot.java
@@ -0,0 +1,29 @@
+package com.techyourchance.mvc.common.di;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+
+import com.techyourchance.mvc.networking.StackoverflowApi;
+
+/**
+ * This is tied to the activity lifecycle, every object needed to instantiate MVC views
+ * can be found here
+ */
+public class ControllerCompositionRoot {
+
+    private final CompositionRoot compositionRoot;
+    private Activity activity;
+
+    public ControllerCompositionRoot(CompositionRoot compositionRoot, Activity activity) {
+        this.compositionRoot = compositionRoot;
+        this.activity = activity;
+    }
+
+    public StackoverflowApi provideStackOverflowApi() {
+        return compositionRoot.provideStackOverflowApi();
+    }
+
+    private LayoutInflater provideLayoutInflater() {
+        return LayoutInflater.from(activity);
+    }
+}
--- app/src/main/java/com/techyourchance/mvc/screens/common/BaseActivity.java
+++ app/src/main/java/com/techyourchance/mvc/screens/common/BaseActivity.java
@@ -3,12 +3,20 @@ package com.techyourchance.mvc.screens.common;
 import android.support.v7.app.AppCompatActivity;
 
 import com.techyourchance.CustomApplication;
-import com.techyourchance.mvc.common.di.CompositionRoot;
+import com.techyourchance.mvc.common.di.ControllerCompositionRoot;
 
 public class BaseActivity extends AppCompatActivity {
 
-    protected CompositionRoot getCompositionRoot() {
-        return ((CustomApplication) getApplication()).getCompositionRoot();
+    private ControllerCompositionRoot controllerCompositionRoot;
+
+    protected ControllerCompositionRoot getCompositionRoot() {
+        if(controllerCompositionRoot == null) {
+            controllerCompositionRoot = new ControllerCompositionRoot(
+                    ((CustomApplication) getApplication()).getCompositionRoot(),
+                    this
+            );
+        }
+        return controllerCompositionRoot;
     }
 
 }
